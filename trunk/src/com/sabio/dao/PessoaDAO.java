229f1043c5b355afcb7605751cf3955d5435afa1
saulo.quad@gmail.com <saulo.quad@gmail.com@e80055fe-911a-1e57-08ca-14d0463027bc>
2012-08-04 03:21:42 +0000

    parte do cadastro de pessoa

--- trunk/src/com/sabio/dao/PessoaDAO.java
+++ trunk/src/com/sabio/dao/PessoaDAO.java
@@ -1,5 +1,56 @@
 package com.sabio.dao;
 
-public interface PessoaDAO {
+import java.util.List;
 
+import com.sabio.bean.Pessoa;
+
+public interface PessoaDAO {
+	/**
+	 * Insere uma etnia no banco de dados
+	 * @param etnia
+	 * @return true se obeter sucesso, ou false se falhar.
+	 * @throws Exception 
+	 * @throws Exception
+	 */
+	public boolean inserir(Pessoa pessoa) throws  Exception ;
+	
+	/**
+	 * Altera uma etnia no banco de dados
+	 * @param etnia
+	 * @return
+	 * @throws Exception
+	 */
+	public boolean alterar(Pessoa pessoa) throws  Exception;
+	
+	/**
+	 * Excluir uma etnia no banco de dados
+	 * @param etnia
+	 * @return
+	 * @throws ExcluirEntidadeException 
+	 * @throws Exception
+	 */
+	public boolean excluir(Pessoa pessoa) throws Exception;
+	
+	/**
+	 * Retorna uma Lista (List) com todas as etnias, começando com o parametro CNPJ ou descrição ou sigla.
+	 * Se campos forem branco, lista todas as etnias
+	 * @param CNPJ
+	 * @param descrição
+	 * @param sigla
+	 * @return List
+	 */
+	public List<Pessoa> getSelecionar(String CPF, String RG, String nome);
+	
+	/**
+	 * Retorna uma instacia de Pessoa com o codigoPessoa passado no parametros
+	 * @param codigoPessoa
+	 * @return Pessoa
+	 */
+	public Pessoa getPessoa(int codigoPessoa);
+	
+	/**
+	 * Retorna uma lista de todas as etnias
+	 * @return List
+	 */
+	public List<Pessoa> getListTodos();
 }
--- trunk/src/com/sabio/servico/cadastro/PessoaServico.java
+++ trunk/src/com/sabio/servico/cadastro/PessoaServico.java
@@ -2,32 +2,32 @@ package com.sabio.servico.cadastro;
 
 import java.util.List;
 
-import com.sabio.bean.Etnia;
+import com.sabio.bean.Pessoa;
 import com.sabio.dao.DAOFactory;
 import com.sabio.servico.validador.GeraMensagem;
 import com.sabio.servico.validador.ValidadorFactory;
 import com.sabio.servico.validador.Validar;
 
-public class EtniaServico {
+public class PessoaServico {
 
-	private Etnia etnia;
+	private Pessoa pessoa;
 	private String mensagem;
 
-	public EtniaServico() {
+	public PessoaServico() {
 	}
 
-	public List<Etnia> pesquisar(String pesquisaDescricao){
-		return DAOFactory.getEtniaDAO().getSelecionar(pesquisaDescricao);
+	public List<Pessoa> pesquisar(String CPF, String RG, String nome){
+		return DAOFactory.getPessoaDAO().getSelecionar(CPF, RG, nome);
 	}
 
 	/**
-	 * Verifica se pode grava o Etnia se sim chama incluir
+	 * Verifica se pode grava o Pessoa se sim chama incluir
 	 * 
 	 * @param aluno
 	 * @throws Exception
 	 */
-	public void gravar(Etnia etnia) throws Exception{
-		this.etnia = etnia;
+	public void gravar(Pessoa pessoa) throws Exception{
+		this.pessoa = pessoa;
 		if (!validaObjeto()){
 			throw new Exception(mensagem);  
 		}
@@ -45,7 +45,7 @@ public class EtniaServico {
 		boolean ok = true;
 				
 		Validar validador = ValidadorFactory.getValidador();
-		validador.validar(etnia);
+		validador.validar(pessoa);
 		String[] mensagens = validador.getMensagensValidacao();
 		mensagem = GeraMensagem.getMensagem(mensagens);
 
@@ -65,10 +65,10 @@ public class EtniaServico {
 		boolean ok = true;
 
 		try {
-			if(etnia.getCodigoEtnia() == 0 ){
-				DAOFactory.getEtniaDAO().inserir(etnia);
+			if(pessoa.getCodigoPessoa() == 0 ){
+				DAOFactory.getPessoaDAO().inserir(pessoa);
 			}else{
-				DAOFactory.getEtniaDAO().alterar(etnia);
+				DAOFactory.getPessoaDAO().alterar(pessoa);
 			}
 		} catch (Exception e) {
 			ok = false;
@@ -79,19 +79,19 @@ public class EtniaServico {
 		return ok;
 	}
 
-	public void excluir(Etnia etnia) throws Exception{
+	public void excluir(Pessoa pessoa) throws Exception{
 		try{
-			DAOFactory.getEtniaDAO().excluir(etnia);
+			DAOFactory.getPessoaDAO().excluir(pessoa);
 		}catch (Exception e){
 			throw new Exception("Erro na exclus‹o de Aluno.");  
 		}
 	}
 
-	public Etnia getEtnia() {
-		return etnia;
+	public Pessoa getPessoa() {
+		return pessoa;
 	}
 
-	public void setEtnia(Etnia etnia) {
-		this.etnia = etnia;
+	public void setPessoa(Pessoa pessoa) {
+		this.pessoa = pessoa;
 	}
 }
