7df695c45a7dca3c7cee352c34d0d9b74f00ae98
saulo.quad <saulo.quad@gmail.com>
2012-08-08 22:01:35 +0000

    Ajustes

--- src/com/sabio/servico/cadastro/GrauParentescoServico.java
+++ src/com/sabio/servico/cadastro/GrauParentescoServico.java
@@ -1,5 +1,6 @@
 package com.sabio.servico.cadastro;
 
+import java.io.Serializable;
 import java.util.List;
 
 import com.sabio.bean.GrauParentesco;
@@ -8,8 +9,8 @@ import com.sabio.servico.validador.GeraMensagem;
 import com.sabio.servico.validador.ValidadorFactory;
 import com.sabio.servico.validador.Validar;
 
-public class GrauParentescoServico {
-
+public class GrauParentescoServico implements Serializable{
+	private static final long serialVersionUID = 1L;
 	private GrauParentesco grauParentesco;
 	private String mensagem;
 
